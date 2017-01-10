64c735327b1d7558215efe6a35028205b7cf30a9
mhq0123 <532386274@qq.com>
2017-01-10 23:19:16 +0800

    添加校验框架

--- springleaf-core/src/main/java/org/mhq0123/springleaf/core/SpringleafCoreConstants.java
+++ springleaf-core/src/main/java/org/mhq0123/springleaf/core/SpringleafCoreConstants.java
@@ -12,6 +12,6 @@ public class SpringleafCoreConstants {
     /** 跨域*/
     public final static class Cors {
         /** 换行符*/
-        public static final String MAPPING = "/api/*/**";
+        public static final String MAPPING = "/*/**";
     }
 }
--- springleaf-core/src/main/java/org/mhq0123/springleaf/core/adapter/CorsConfigurerAdapter.java
+++ springleaf-core/src/main/java/org/mhq0123/springleaf/core/adapter/CorsConfigurerAdapter.java
@@ -1,14 +1,12 @@
 package org.mhq0123.springleaf.core.adapter;
 
 import org.mhq0123.springleaf.core.SpringleafCoreConstants;
-import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 /**
  * 全局跨域
  */
-@Configurable
 public class CorsConfigurerAdapter extends WebMvcConfigurerAdapter {
 
 	@Override
