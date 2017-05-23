74802a9f28090ee5ae4de8ef6365d1e5fa81af6d
mhq0123 <mahuiquan@colotnet.com>
2017-05-23 16:14:19 +0800

    完善获取ip工具类

--- springleaf-common/src/main/java/org/mhq0123/springleaf/common/utils/InetUtils.java
+++ springleaf-common/src/main/java/org/mhq0123/springleaf/common/utils/InetUtils.java
@@ -5,6 +5,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.util.Enumeration;
+import java.util.regex.Pattern;
 
 /**
  * @author mhq0123
@@ -19,7 +22,7 @@ public class InetUtils {
 
     private InetUtils(){}
 
-    private static String hostAddress = null;
+    private static volatile InetAddress LOCAL_ADDRESS = null;
 
     /** 截取ip最后一段，补足3位长度*/
     private static String lastSplitIp_3l = null;
@@ -27,20 +30,78 @@ public class InetUtils {
     /** 截取ip最后两段，补足6位长度*/
     private static String lastSplitIp_6l = null;
 
+    private static final Pattern IP_PATTERN = Pattern.compile("\\d{1,3}(\\.\\d{1,3}){3,5}$");
+
     /**
      * 获取ip
      * @return
      */
-    public static String getHostAddress() {
-        try {
-            if(StringUtils.isBlank(hostAddress)) {
-                hostAddress = InetAddress.getLocalHost().getHostAddress();
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
+    public static String getLocalHost() {
+        InetAddress address = getLocalAddress();
+        return address == null ? "127.0.0.1" : address.getHostAddress();
+    }
+
+    public static InetAddress getLocalAddress() {
+        if(LOCAL_ADDRESS != null) {
+            return LOCAL_ADDRESS;
+        } else {
+            InetAddress localAddress = getLocalAddress0();
+            LOCAL_ADDRESS = localAddress;
+            return localAddress;
         }
-        logger.info(">>>>>>>>>>>>>>get host address:{}", hostAddress);
-        return hostAddress;
+    }
+
+    private static InetAddress getLocalAddress0() {
+        InetAddress localAddress = null;
+
+        try {
+            localAddress = InetAddress.getLocalHost();
+            if(isValidAddress(localAddress)) {
+                return localAddress;
+            }
+        } catch (Throwable var6) {
+            logger.warn("Failed to retriving ip address, " + var6.getMessage(), var6);
+        }
+
+        try {
+            Enumeration<NetworkInterface> interfaces = NetworkInterface.getNetworkInterfaces();
+            if(interfaces != null) {
+                while(interfaces.hasMoreElements()) {
+                    try {
+                        NetworkInterface network = (NetworkInterface)interfaces.nextElement();
+                        Enumeration<InetAddress> addresses = network.getInetAddresses();
+                        if(addresses != null) {
+                            while(addresses.hasMoreElements()) {
+                                try {
+                                    InetAddress address = (InetAddress)addresses.nextElement();
+                                    if(isValidAddress(address)) {
+                                        return address;
+                                    }
+                                } catch (Throwable var5) {
+                                    logger.warn("Failed to retriving ip address, " + var5.getMessage(), var5);
+                                }
+                            }
+                        }
+                    } catch (Throwable var7) {
+                        logger.warn("Failed to retriving ip address, " + var7.getMessage(), var7);
+                    }
+                }
+            }
+        } catch (Throwable var8) {
+            logger.warn("Failed to retriving ip address, " + var8.getMessage(), var8);
+        }
+
+        logger.error("Could not get local host ip address, will use 127.0.0.1 instead.");
+        return localAddress;
+    }
+
+    private static boolean isValidAddress(InetAddress address) {
+        if(address != null && !address.isLoopbackAddress()) {
+            String name = address.getHostAddress();
+            return name != null && !"0.0.0.0".equals(name) && !"127.0.0.1".equals(name) && IP_PATTERN.matcher(name).matches();
+        } else {
+            return false;
+        }
     }
 
     /**
